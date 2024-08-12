f63555ebe69b4349c84528f5722e8a6f58e99c1a
lokii-space <venkatlokesh79957@gmail.com>
2024-08-12 18:47:24 +0530

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -73,7 +73,7 @@ const Register = () => {
               <div>
                 <input
                   type="text"
-                  placeholder="Zeeshan"
+                  placeholder="Lokesh"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
@@ -85,7 +85,7 @@ const Register = () => {
               <div>
                 <input
                   type="email"
-                  placeholder="zk@gmail.com"
+                  placeholder="lokii@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
